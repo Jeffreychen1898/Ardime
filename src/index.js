84326667418e6289aa8055e8372feef09adbb046
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Air.local>
2023-03-01 00:10:17 -0600

    added transformation to list of exports

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {Renderer} from "./renderer/renderer.js";
 import Image from "./renderer/image.js";
 import Performance from "./utils/performance.js";
 import * as Constants from "./utils/constants.js";
+import * as Transformation from "./geometry/transformation.js";
 
 /* @param { String, math.js_library } */
 function useLibrary(_libraryType, _library) {
@@ -12,9 +13,14 @@ function useLibrary(_libraryType, _library) {
 	}
 }
 
+const Geometry = {
+	Transformation
+};
+
 export {
 	Renderer,
 	Image,
 	Performance,
-	useLibrary
+	useLibrary,
+	Geometry
 };
